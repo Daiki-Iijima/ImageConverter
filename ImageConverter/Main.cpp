33edacd616c0344c40fbda72eac4348bc5638a4e
daiki_iijima <daiki.applecreate@gmail.com>
2019-12-19 02:59:50 +0900

    メソッド切り出し

--- ImageConverter/Main.cpp
+++ ImageConverter/Main.cpp
@@ -15,18 +15,28 @@ void TitleDraw()
 
 }
 
-
-void loadFile()
+int HexCharToInt(char* _char)
 {
-	string filename = "test.bmp";
-	ifstream ifs(filename, ios::binary);//	バイナリ形式で読み取り
+	string hexStr(_char, sizeof(_char) / sizeof(_char[0]));	//	16進でサイズが入る
 
-	filename.insert(filename.size() - 4, "new");
-	ofstream ofs(filename.c_str(), ios::binary);		//	バイナリ形式で書き出し
+	char hexChar[100];
 
-	if (!ofs) {        //ターゲットファイルが開けなかったら終了
-		cout << "file open error" << endl;
-	}
+	sprintf_s(hexChar, "%x", hexStr);						//	16進のchar配列型に変換
+
+	int octInt = std::stoi(hexChar, nullptr, 16);			// 16進を10進に変換してint型に代入
+
+	return octInt;
+}
+
+//	INFOHEADERの情報を表示
+//	TODO : 取得できるようにするのもいいかも
+bool LoadInfoData(string _filename)
+{
+	ifstream ifs(_filename, ios::binary);//	バイナリ形式で読み取り
+
+	if (!ifs) return false;        //ターゲットファイルが開けなかったら終了
+
+	cout << "=== 正常に読み取りました ===" << endl;
 
 	char temp;
 	char tempSize[4];
@@ -35,6 +45,7 @@ void loadFile()
 	char tempInfoHeaderSize[4];
 	unsigned char tempWidth[4];
 	unsigned char tempHeight[4];
+	unsigned char tempPlaneCount[2];
 	unsigned char tempPixelCount[2];
 	unsigned char tempCompressionFormat[2];
 	unsigned char tempCompressionSize[2];
@@ -43,6 +54,102 @@ void loadFile()
 	unsigned char tempColorCount[4];
 	unsigned char tempImportantColorCountmp[4];
 
+	//	ヘッダ部:54byte
+	for (int i = 0; i < 54; i++)	//	BITMAPFILEHEADER(ヘッダー情報)と、BITMAPINFOHEADER()をコピー
+	{
+		ifs.get(temp);
+
+		if (i >= 0 && i <= 1)		//	フォーマットの種類
+			tempFormat[i] = temp;
+
+		if (i >= 2 && i <= 5)		//	2 - 5ビットはファイルサイズ(byte)
+			tempSize[i - 2] = temp;
+
+		if (i >= 10 && i <= 13)		//	ヘッダサイズ
+			tempHeaderSize[i - 10] = temp;
+
+		if (i >= 14 && i <= 17)		//	情報ヘッダサイズ
+			tempInfoHeaderSize[i - 14] = temp;
+
+		if (i >= 18 && i <= 21)		//	画像の幅
+			tempWidth[i - 18] = temp;
+
+		if (i >= 22 && i <= 25)		//	画像の高さ
+			tempHeight[i - 22] = temp;
+
+		if(i >= 26 && i <= 27)		//	画像の高さ
+			tempPlaneCount[i - 26] = temp;
+
+		if (i >= 28 && i <= 29)		//	画素数
+			tempPixelCount[i - 28] = temp;
+
+		if (i >= 30 && i <= 33)		//	圧縮形式
+			tempCompressionFormat[i - 30] = temp;
+
+		if (i >= 34 && i <= 37)		//	圧縮サイズ
+			tempCompressionSize[i - 34] = temp;
+
+		if (i >= 38 && i <= 41)		//	水平解像度
+			tempHorizontalCompression[i - 38] = temp;
+
+		if (i >= 42 && i <= 45)		//	垂直解像度
+			tempVerticalCompression[i - 42] = temp;
+
+		if (i >= 46 && i <= 49)		//	色数
+			tempColorCount[i - 46] = temp;
+
+		if (i >= 50 && i <= 53)		//	重要色数
+			tempImportantColorCountmp[i - 50] = temp;
+	}
+
+	printf("フォーマット : %c%c\n", tempFormat[0], tempFormat[1]);				//	ファイル形式
+
+	int width = HexCharToInt((char*)tempWidth);									// ファイル横サイズ
+	printf("幅 : %d\n", width);
+
+	int height = HexCharToInt((char*)tempHeight);								// ファイル縦サイズ
+	printf("高さ : %d\n", height);
+
+	int size = HexCharToInt(tempSize);											// サイズの算出
+	printf("ファイルサイズ : %d\n", size);
+
+	printf("プレーン数 : %d\n", tempPlaneCount[0]);								// プレーン数の算出
+
+	printf("ピクセル深度 : %d\n", tempPixelCount[0]);							// 画素数の算出
+
+	printf("圧縮形式 : %d\n", tempCompressionFormat[0]);						// 圧縮形式の算出
+
+	int compressionSize = HexCharToInt((char*)tempCompressionSize);				// 圧縮サイズの算出
+	printf("圧縮サイズ : %d\n", compressionSize);
+
+	int horizontalCompression = HexCharToInt((char*)tempHorizontalCompression);	// 水平解像度の算出
+	printf("水平解像度 : %d\n", horizontalCompression);
+
+	int verticalCompression = HexCharToInt((char*)tempVerticalCompression);		// 垂直解像度の算出
+	printf("垂直解像度 : %d\n", verticalCompression);
+
+	int colorCount = HexCharToInt((char*)tempColorCount);						// カラーインデックス数の算出
+	printf("カラーインデックス数 : %d\n", colorCount);
+	
+	int importantColorCountmp = HexCharToInt((char*)tempImportantColorCountmp);	// 重要インデックス数
+	printf("重要インデックス数 : %d\n", importantColorCountmp);
+
+	return true;
+}
+
+
+void ChangeColor(string _filename,int _r,int _g,int _b,int _cr, int _cg, int _cb)
+{
+	ifstream ifs(_filename, ios::binary);//	バイナリ形式で読み取り
+
+	_filename.insert(_filename.size() - 4, "new");
+	ofstream ofs(_filename.c_str(), ios::binary);		//	バイナリ形式で書き出し
+
+	if (!ofs) {        //ターゲットファイルが開けなかったら終了
+		cout << "file open error" << endl;
+	}
+
+	char temp;
 	char rgb[4];
 
 	//	ヘッダ部:54byte
@@ -50,210 +157,18 @@ void loadFile()
 	{
 		ifs.get(temp);
 		ofs << temp;	//	アウトプットストリームにそのまま書き込む
-		if (i >= 0 && i <= 1)		//	フォーマットの種類
-		{
-			tempFormat[i] = temp;
-		}
-
-		if (i >= 2 && i <= 5)		//	2 - 5ビットはファイルサイズ(byte)
-		{
-			tempSize[i - 2] = temp;
-		}
-
-		if (i >= 10 && i <= 13)		//	ヘッダサイズ
-		{
-			tempHeaderSize[i - 10] = temp;
-		}
-
-		if (i >= 14 && i <= 17)		//	情報ヘッダサイズ
-		{
-			tempInfoHeaderSize[i - 14] = temp;
-		}
-
-		if (i >= 18 && i <= 21)		//	画像の幅
-		{
-			tempWidth[i - 18] = temp;
-		}
-		if (i >= 22 && i <= 25)		//	画像の高さ
-		{
-			tempHeight[i - 22] = temp;
-		}
-		//	26,27はプレーン数(1)
-
-		if (i >= 28 && i <= 29)		//	画素数
-		{
-			tempPixelCount[i - 28] = temp;
-		}
-
-		if (i >= 30 && i <= 33)		//	圧縮形式
-		{
-			tempCompressionFormat[i - 30] = temp;
-		}
-
-		if (i >= 34 && i <= 37)		//	圧縮サイズ
-		{
-			tempCompressionSize[i - 34] = temp;
-		}
-
-		if (i >= 38 && i <= 41)		//	水平解像度
-		{
-			tempHorizontalCompression[i - 38] = temp;
-		}
-
-		if (i >= 42 && i <= 45)		//	垂直解像度
-		{
-			tempVerticalCompression[i - 42] = temp;
-		}
-
-		if (i >= 46 && i <= 49)		//	色数
-		{
-			tempColorCount[i - 46] = temp;
-		}
-		if (i >= 50 && i <= 53)		//	重要色数
-		{
-			tempImportantColorCountmp[i - 50] = temp;
-		}
-
-		
 	}
 
-	//	===	ファイル形式 ===
-	printf("フォーマット:%c%c\n", tempFormat[0], tempFormat[1]);
-	//	====================
-
-	//	=== ファイル縦横サイズ ===
-
-	string hexWidthStr((char*)tempWidth, sizeof(tempWidth) / sizeof(tempWidth[0]));
-	string hexHeightStr((char*)tempHeight, sizeof(tempHeight) / sizeof(tempHeight[0]));
-
-	char hexWidth[100];
-	char hexHeight[100];
-
-	sprintf_s(hexWidth, "%x", hexWidthStr);									//	16進のchar配列型に変換
-	sprintf_s(hexHeight, "%x", hexHeightStr);									//	16進のchar配列型に変換
-
-	int width = std::stoi(hexWidth, nullptr, 16);								// 16進を10進に変換してint型に代入
-	int height = std::stoi(hexHeight, nullptr, 16);								// 16進を10進に変換してint型に代入
-
-	printf("幅:%d\n", width);
-	printf("高さ:%d\n", height);
-
-	//	==========================
-
-	//	===	サイズの算出 ===
-	string hexSizeStr(tempSize, sizeof(tempSize) / sizeof(tempSize[0]));	//	16進でサイズが入る
-
-	char hexSize[100];
-
-	sprintf_s(hexSize, "%x", hexSizeStr);									//	16進のchar配列型に変換
-
-	int size = std::stoi(hexSize, nullptr, 16);								// 16進を10進に変換してint型に代入
-
-	printf("Size:%d\n", size);
-	//	====================
-
-	//	=== 画素数の算出 ===
-	for (int i = 0; i < sizeof(tempPixelCount); i++)
-	{
-		printf("ピクセル深度 : %d\n", tempPixelCount[i]);
-	}
-	//	====================
-
-	//	=== 圧縮形式の算出 ===
-	for (int i = 0; i < sizeof(tempCompressionFormat); i++)
-	{
-		printf("圧縮形式 : %x\n", tempCompressionFormat[i]);
-	}
-	//	======================
-
-	//	=== 圧縮サイズの算出 ===
-	string hexCompressionSizeStr((char*)tempCompressionSize, sizeof(tempCompressionSize) / sizeof(tempCompressionSize[0]));	//	16進でサイズが入る
-
-	char hexCompressionSize[100];
-
-	sprintf_s(hexCompressionSize, "%x", hexSizeStr);									//	16進のchar配列型に変換
-
-	int compressionSize = std::stoi(hexCompressionSize, nullptr, 16);								// 16進を10進に変換してint型に代入
-
-	printf("圧縮サイズ : %d\n", compressionSize);
-	//	======================
-
-//	=== 水平解像度の算出 ===
-	string hexHorizontalCompressionStr((char*)tempHorizontalCompression, sizeof(tempHorizontalCompression) / sizeof(tempHorizontalCompression[0]));	//	16進でサイズが入る
-
-	char hexHorizontalCompression[100];
-
-	sprintf_s(hexHorizontalCompression, "%x", hexHorizontalCompressionStr);									//	16進のchar配列型に変換
-
-	int horizontalCompression = std::stoi(hexHorizontalCompression, nullptr, 16);								// 16進を10進に変換してint型に代入
-
-	printf("水平解像度 : %d\n", horizontalCompression);
-	//	======================
-
-//	=== 垂直解像度の算出 ===
-	string hexVerticalCompressionStr((char*)tempVerticalCompression, sizeof(tempVerticalCompression) / sizeof(tempVerticalCompression[0]));	//	16進でサイズが入る
-
-	char hexVerticalCompression[100];
-
-	sprintf_s(hexVerticalCompression, "%x", hexVerticalCompressionStr);									//	16進のchar配列型に変換
-
-	int verticalCompression = std::stoi(hexVerticalCompression, nullptr, 16);								// 16進を10進に変換してint型に代入
-
-	printf("垂直解像度 : %d\n", verticalCompression);
-	//	======================
-	
-	
 	int i = 0;
 	while (ifs.get(temp)) {     //後はファイル終了まで繰り返し
 		rgb[i] = temp;
 		i++;
 		if (i == 3) {
 			i = 0;
-			if (rgb[0] == -1 && rgb[1] == -1 && rgb[2] == -1) {//もし白(全部-1)なら
-				rgb[0] = 192;    //B
-				rgb[1] = 192;    //G
-				rgb[2] = 192;    //R
-			}
-			ofs << rgb[0] << rgb[1] << rgb[2];
-		}
-	}
-}
-
-
-void ChangeColor()
-{
-
-	string filename = "test.bmp";
-
-	ifstream ifs(filename, ios::binary);
-	if (!ifs) {        //ソースファイルが開けなかったら終了
-		cout << "file open error" << endl;
-	}
-
-	filename.insert(filename.size() - 4, "new");
-	ofstream ofs(filename.c_str(), ios::binary);
-	if (!ofs) {        //ターゲットファイルが開けなかったら終了
-		cout << "file open error" << endl;
-	}
-
-	int i;
-	char temp;
-	char rgb[4];
-
-	for (i = 0; i < 54; i++) {     //BITMAPFILEHEADERとBITMAPINFOHEADERを丸々コピー
-		ifs.get(temp);
-		ofs << temp;
-	}
-	i = 0;
-	while (ifs.get(temp)) {     //後はファイル終了まで繰り返し
-		rgb[i] = temp;
-		i++;
-		if (i == 3) {
-			i = 0;
-			if (rgb[0] == -1 && rgb[1] == -1 && rgb[2] == -1) {//もし白(全部-1)なら
-				rgb[0] = 192;    //B
-				rgb[1] = 192;    //G
-				rgb[2] = 192;    //R
+			if (rgb[0] == _r && rgb[1] == _g && rgb[2] == _b) {//もし指定色なら
+				rgb[0] = _cr;    //B
+				rgb[1] = _cg;    //G
+				rgb[2] = _cb;    //R
 			}
 			ofs << rgb[0] << rgb[1] << rgb[2];
 		}
@@ -263,9 +178,24 @@ void ChangeColor()
 int main()
 {
 	TitleDraw();	//	タイトル画面
+	cout << "ファイル名を入力してください" << endl;
+	string _filename;
+	cin >> _filename;
 
-	loadFile();		//	画像情報
-	//ChangeColor();
+	if (LoadInfoData(_filename))//	画像情報
+	{
+		cout << "============================" << endl;
+		getchar();
+	}
+	else
+	{
+		cout << "読み取りに失敗しました" << endl;
+		getchar();
+	}
+
+	cout << "変更したい色を指定してください" << endl;
+
+	ChangeColor(_filename, -1, -1, -1, 1, 1, 1);
 
 	getchar();
 	return 0;
